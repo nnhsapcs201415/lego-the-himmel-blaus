bba44be78e4b7943e03a61cf4f04f58c02fb187b
atrafacz <alexrafacz@gmail.com>
2017-05-16 15:03:34 -0500

    finish the touch sensor

---
+++ Sensors/LightTest.java
@@ -0,0 +1,17 @@
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.Button;
+
+public class LightTest {
+    public static void main(String[] args) throws Exception {
+        LightSensor light = new LightSensor(SensorPort.S2);
+
+        while (!Button.ESCAPE.isDown()) {
+            LCD.drawInt(light.getLightValue(), 4, 0, 0);
+            LCD.drawInt(light.getNormalizedLightValue(), 4, 0, 1);
+            LCD.drawInt(SensorPort.S2.readRawValue(), 4, 0, 2);
+            LCD.drawInt(SensorPort.S2.readValue(), 4, 0, 3);
+        }
+    }
+}
--- Sensors/TouchTest.java
+++ Sensors/TouchTest.java
@@ -1,6 +1,9 @@
 import lejos.nxt.LCD;
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class TouchTest {
     public static void main(String[] args) throws Exception {
