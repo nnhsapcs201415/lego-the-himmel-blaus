a82c47cadf7a9cf974761c360454444c4493464d
ksrajagopal <ksrajagopal@stu.naperville203.org>
2017-05-17 15:08:49 -0500

    finish touchsensor

--- Sensors/TouchTest.java
+++ Sensors/TouchTest.java
@@ -1,14 +1,13 @@
 import lejos.nxt.LCD;
-import lejos.nxt.Button;
-import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TouchSensor;
-import lejos.robotics.navigation.DifferentialPilot;
 
 public class TouchTest {
     public static void main(String[] args) throws Exception {
         TouchSensor touch = new TouchSensor(SensorPort.S1);
-        while (!touch.isPressed()){}
+        while (!touch.isPressed()) {
+            
+        }
         LCD.drawString("Finished", 3, 4);
     }
 }
