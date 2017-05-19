5013d508b02d2ae3684b4b68f96b3efdaf453227
atrafacz <alexrafacz@gmail.com>
2017-05-19 14:50:28 -0500

    alec still needs to clone it

--- Motors/TouchTest.java
+++ Motors/TouchTest.java
@@ -5,13 +5,13 @@ import lejos.nxt.TouchSensor;
 public class TouchTest {
     public static void main(String[] args) throws Exception {
         TouchSensor touch = new TouchSensor(SensorPort.S1);
-        if(touch.isPressed()) {
-            String[] list = new String[0];
-            MotorTutor2.main(list);
+        if(!touch.isPressed()) {
+
         }
         else
         {
-            
+            String[] list = new String[0];
+            MotorTutor2.main(list);
         }
         LCD.drawString("Finished", 3, 4);
     }
