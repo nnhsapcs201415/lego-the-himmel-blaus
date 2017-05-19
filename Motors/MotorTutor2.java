d053661d16737bd56a086a56fd9ec9d46e8b2724
atrafacz <alexrafacz@gmail.com>
2017-05-19 14:49:35 -0500

    alec needs to clone it

--- Motors/MotorTutor2.java
+++ Motors/MotorTutor2.java
@@ -13,13 +13,14 @@ public class MotorTutor2 {
     {
         LCD.drawString("Program 2", 0, 0);
         Button.waitForAnyPress();
-        Motor.A.setSpeed(180);
-        Motor.A.forward();
+        Motor.A.setSpeed(160);
+        Motor.A.backward();
+        //Button.waitForAnyPress();
         //LCD.clear();
-        Delay.msDelay(2000);
+        Delay.msDelay(1000);
         LCD.drawInt(Motor.A.getTachoCount(),0,0);
         LCD.drawInt(Motor.A.getTachoCount(),0,1);
-        Motor.A.backward();
+        Motor.A.forward();
         //while (Motor.A.getTachoCount()>0); 
         LCD.drawInt(Motor.A.getTachoCount(),0,2);
         //Motor.A.stop();
--- Motors/TouchTest.java
+++ Motors/TouchTest.java
@@ -5,9 +5,13 @@ import lejos.nxt.TouchSensor;
 public class TouchTest {
     public static void main(String[] args) throws Exception {
         TouchSensor touch = new TouchSensor(SensorPort.S1);
-        while (!touch.isPressed()) {
+        if(touch.isPressed()) {
             String[] list = new String[0];
             MotorTutor2.main(list);
+        }
+        else
+        {
+            
         }
         LCD.drawString("Finished", 3, 4);
     }
